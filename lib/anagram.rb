3587da5628b6827b6119261e6fdc83d5546d2884
Virginia Dooley <vcdooley@gmail.com>
2018-10-24 19:35:23 +0200

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word= word
+  end
+
+  def match(array_of_words)
+    array_of_words.select do |element| #iterates over an array of words
+     (@word.split("").sort) == (element.split("").sort) #selects and splits each word in the array and sorts the letters in that word
+    end
+  end
+
+end
